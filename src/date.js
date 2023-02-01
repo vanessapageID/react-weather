36ff8bb72f43c2ae840cd302af5577a6d598fbe6
rlmegj <103387789+rlmegj@users.noreply.github.com>
2023-02-01 16:21:45 +0100

    Created app

---
+++ src/date.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Date(props) {
+  //let date = [props.date];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
+}
--- src/search.js
+++ src/search.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 //import Weather from "./weather.js";
 import axios from "axios";
+import Date from "./date";
 
 export default function Search() {
-  //let [weather, setWeather] = useState(null);
   let [city, setCity] = useState(null);
   let [temperature, setTemperature] = useState(null);
   let [sky, setSky] = useState(null);
@@ -12,7 +12,7 @@ export default function Search() {
   let [icon, setIcon] = useState("");
   let [rain, setRain] = useState("");
   let [cityName, setCityName] = useState("");
-  //let [city] = "Hello";
+  let [date, setDate] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -26,6 +26,7 @@ export default function Search() {
   }
 
   function showWeather(response) {
+    setDate(response.data.dt * 1000);
     setRain(response.data.weather[0].main);
     setTemperature(Math.round(response.data.main.temp * 10) / 10);
     setSky(response.data.weather[0].description);
@@ -42,18 +43,21 @@ export default function Search() {
       <div>
         <div>
           <div className="Forecast">
-            <h2>Currently, in {cityName}:</h2>
-            <p className="rain"> It is raining.</p>
-            <table>
-              <tr>
-                <td> The temperature is {temperature}°C</td>
-                <td>The sky is {sky}</td>
-              </tr>
-              <tr>
-                <td>There is {humidity}% humidity</td>
-                <td>The wind speed is {wind} km/h</td>
-              </tr>
-            </table>
+            <h2>Currently, in {cityName}</h2>
+            <h3>
+              <Date date={date} />
+            </h3>
+            <p className="rain"> it is raining.</p>
+            <div className="container">
+              <div className="row">
+                <div className="col-6">The temperature is {temperature}°C</div>
+                <div className="col-6">The sky is {sky}</div>
+              </div>
+              <div className="row">
+                <div className="col-6">There is {humidity}% humidity</div>
+                <div className="col-6">The wind speed is {wind} km/h</div>
+              </div>
+            </div>
             <p>
               <img src={icon} alt="weather icon" />
             </p>
@@ -62,7 +66,7 @@ export default function Search() {
             <form onSubmit={handleSubmit}>
               <input
                 type="search"
-                Placeholder="Enter a city & press Search"
+                placeholder="Enter a city & press Search"
                 onChange={updateCity}
               />
               <input className="button" type="submit" value="Search" />
@@ -76,18 +80,21 @@ export default function Search() {
       <div>
         <div>
           <div className="Forecast">
-            <h2>Currently, in {cityName}:</h2>
-            <p className="rain">It is not raining.</p>
-            <table>
-              <tr>
-                <td> The temperature is {temperature}°C</td>
-                <td>The sky is {sky}</td>
-              </tr>
-              <tr>
-                <td>There is {humidity}% humidity</td>
-                <td>The wind speed is {wind} km/h</td>
-              </tr>
-            </table>
+            <h2>Currently, in {cityName}</h2>
+            <h3>
+              <Date date={date} />
+            </h3>
+            <p className="rain">it is not raining.</p>
+            <div className="container">
+              <div className="row">
+                <div className="col-6">The temperature is {temperature}°C</div>
+                <div className="col-6">The sky is {sky}</div>
+              </div>
+              <div className="row">
+                <div className="col-6">There is {humidity}% humidity</div>
+                <div className="col-6">The wind speed is {wind} km/h</div>
+              </div>
+            </div>
             <p>
               <img src={icon} alt="weather icon" />
             </p>
@@ -96,7 +103,7 @@ export default function Search() {
             <form onSubmit={handleSubmit}>
               <input
                 type="search"
-                Placeholder="Enter a city & press Search"
+                placeholder="Enter a city & press Search"
                 onChange={updateCity}
               />
               <input className="button" type="submit" value="Search" />
@@ -112,7 +119,7 @@ export default function Search() {
           <form onSubmit={handleSubmit}>
             <input
               type="search"
-              Placeholder="Enter a city & press Search"
+              placeholder="Enter a city & press Search"
               onChange={updateCity}
             />
             <input className="button" type="submit" value="Search" />
